cae831a25bfa25c2c34e8a7a5debdf2dea4190c1
Daffadelicious <31669829+Daffadelicious@users.noreply.github.com>
2023-12-08 23:16:07 -0800

    basic html and css

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        gallery: resolve(__dirname, "src/gallery/index.html"),
       },
     },
   },
